b5623a309e38dca68dabbbb8a27765a046b12b8f
Hongkun Yu <staryhk@gmail.com>
2023-02-26 15:42:26 +0800

    add homebrew repo to tsinghua

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -6,6 +6,11 @@
 export ACTHOME=$HOME
 
 # setup PATH for linuxbrew/homebrew # {{{
+export HOMEBREW_INSTALL_FROM_API=1
+export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
 if [[ "$OSTYPE" == "linux-gnu" ]]; then # only set for Linux
   export PATH="$ACTHOME/.linuxbrew/bin":"$ACTHOME/.linuxbrew/sbin":$PATH
   export MANPATH="$ACTHOME/.linuxbrew/share/man":$MANPATH
