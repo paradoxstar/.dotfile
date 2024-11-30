ee5a03f3b372a773b891499665b90a34ce209e06
Hongkun Yu <staryhk@gmail.com>
2024-11-30 19:52:09 +0800

    add themes install scripts & set git default branch to main

---
+++ zsh/theme/install_themes.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#
+# install_thems.sh
+#
+# Copyright © 2024 Hongkun Yu <staryhk@gmail.com>
+#
+# @AUTHOR:      Hongkun Yu
+# @MAIL:        staryhk@gmail.com
+# @VERSION:     2024-11-30
+#
+
+
+cp ./ys-yhk.zsh-theme $ADOTDIR/bundles/robbyrussell/oh-my-zsh/themes/
